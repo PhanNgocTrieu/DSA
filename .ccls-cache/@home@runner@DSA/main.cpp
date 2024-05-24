0700cc9e4a6e4f00d3bb61fad09df1af7df509cd
Trieu John <pntrieu200799@gmail.com>
2024-05-24 07:52:55 +0000

    update

---
+++ .ccls-cache/@home@runner@DSA/main.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#include "backtracking.h"
+
+int main() {
+    algorithm::backtracking& m_bp = algorithm::backtracking::getInstance();
+    m_bp.execute_problem(2);
+    return 0;
+}
